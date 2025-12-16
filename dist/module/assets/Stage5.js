3f3ea27b43baaef92b0abc55b77d40733a2c64f7
ipota <kiratoki512810@gmail.com>
2025-12-16 17:55:41 +0900

    change stage5

--- dist/module/assets/Stage5.js
+++ dist/module/assets/Stage5.js
@@ -1,2 +1,2 @@
-function t(){return{vertices:[[0,-100],[-150,0],[150,0],[0,100]],edges:[[0,1],[0,2],[1,3],[2,1,1,!0],[2,3],[3,0,2,!0]]}}export{t as stage};
+function u(){return{vertices:Array.from({length:5},(r,t)=>[Math.sin(t*(2*Math.PI)/5)*100,-Math.cos(t*(2*Math.PI)/5)*100]),edges:[[0,1,1,!0],[1,2,1,!0],[2,3,1,!0],[3,4,1,!0],[4,0,1,!0],[4,1,1,!0]]}}export{u as stage};
 //# sourceMappingURL=Stage5.js.map
--- src/stages/Stage5.ts
+++ src/stages/Stage5.ts
@@ -1,22 +1,21 @@
 import { Vertex } from "../Game"
 
 export function stage() {
-    const vertices: Vertex[] = [
-        [0, -100],
-        [-150, 0],
-        [150, 0],
-        [0, 100],
-    ]
+    const vertices: Vertex[] = Array.from({ length: 5 }, (_, i) => [
+        Math.sin((i * (2 * Math.PI)) / 5) * 100,
+        -Math.cos((i * (2 * Math.PI)) / 5) * 100,
+    ])
 
     return {
         vertices,
         edges: [
-            [0, 1],
-            [0, 2],
-            [1, 3],
-            [2, 1, 1, true],
-            [2, 3],
-            [3, 0, 2, true],
+            [0, 1, 1, true],
+            [1, 2, 1, true],
+            [2, 3, 1, true],
+            [3, 4, 1, true],
+            [4, 0, 1, true],
+
+            [4, 1, 1, true],
         ],
     }
 }
