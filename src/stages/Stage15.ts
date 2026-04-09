d2f988bcff8076d155dc9fa6cd47324b7904d9e9
ipota <kiratoki512810@gmail.com>
2026-04-09 14:14:49 +0900

    c

--- src/stages/Stage15.ts
+++ src/stages/Stage15.ts
@@ -1,39 +1,37 @@
 import { GraphData, VertexData } from "../Game/Graph"
 
 export function stage(): GraphData {
-    const vertices: VertexData[] = []
-
-    for (let i = 0; i < 5; i++) {
-        vertices.push([Math.sin(((i - 1) * 2 * Math.PI) / 5) * 120, 20 - Math.cos(((i - 1) * 2 * Math.PI) / 5) * 120])
-    }
-
-    vertices.push([-120, -90], [120, -90])
+    const vertices: VertexData[] = [
+        [0, -100],
+        [-150, -60],
+        [0, 30],
+        [-150, 60],
+        [0, 100],
+        [150, 60],
+        [150, -60],
+        [0, -30],
+    ]
 
     return {
         vertices,
         edges: [
             [0, 1],
-            [1, 2],
-            [2, 3],
-            [3, 4, { arrow: true }],
-            [4, 0],
-
-            [0, 2],
-            [5, 1],
-            [6, 1],
-            [6, 2],
-
-            [0, 3, { multiplicity: 3 }],
-            [0, 5, { arrow: true, multiplicity: 3 }],
+            [0, 3],
+            [0, 5],
             [0, 6],
-
+            [0, 7],
+            [1, 2],
             [1, 3],
-            [1, 4, { arrow: true }],
-
-            [2, 4],
-            [2, 5, { arrow: true }],
-
+            [1, 4],
+            [2, 3],
+            [2, 5],
+            [2, 6],
+            [2, 7],
+            [3, 4],
             [3, 5, { multiplicity: 2 }],
+            [4, 5],
+            [4, 6],
+            [5, 6],
         ],
     }
 }
